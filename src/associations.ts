07a2793d1378590a94cdb3fee9a11cb97a5284d5
Gonzalo García <gonzalo@neoland.es>
2020-11-30 18:52:22 +0100

    * cleaning old things and test files

--- src/associations.ts
+++
@@ -1,6 +0,0 @@
-import { User } from './models/user';
-import { Product } from './models/product';
-import { Sales } from './models/sales';
-
-export const UserAssociation = User.belongsToMany(Product, { through: Sales, foreignKey: 'userId'});
-export const ProductAssociation = Product.belongsToMany(User, { through: Sales, foreignKey: 'productId'});
